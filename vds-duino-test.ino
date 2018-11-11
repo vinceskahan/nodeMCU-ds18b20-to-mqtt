1c2cec8b323b6b261183ede4edbb54e5ba87a19b
Vince Skahan <vinceskahan@gmail.com>
2018-11-11 09:56:42 -0800

    add optional wifi disable

--- vds-duino-test.ino
+++ vds-duino-test.ino
@@ -1,5 +1,9 @@
 /*
+ *  GitHub link => https://github.com/vinceskahan/nodeMCU-ds18b20-to-mqtt
+ *
  *  read a ds18b20 sensor and publish the value with MQTT, with status to the onboard LED and OLED display
+ *
+ *  the wifi/mqtt features may be disabled by grounding nodeMCU pin D6 (GPIO12)
  *
  * references:
  *   https://techtutorialsx.com/2017/04/09/esp8266-connecting-to-mqtt-broker/
@@ -11,7 +15,10 @@
  *    - the adafruit ssd1306_128x32_i2c.ino example sketch from their library
  *    - my local tweaks, modifications, and crimes against arduino best practices
  *
- * all bugs are mine, please don't bother the nice folks at Adafruit who wrote the code this is based on... 
+ * all bugs are mine, please don't bother the nice folks at Adafruit who wrote the code this is based on...
+ *
+ * there are undoubtedly copyright notices that I failed to cut+paste into this, but suffice it to say
+ * that the code I learned from (mentioned above) is much appreciated
  *
  * ----------------------------------------------
  *
@@ -21,7 +28,7 @@
  *     GRD to ground rail on breadboard
  *     VCC to power rail on breadboard
  *     DAT to GPIO2 (nodeMCU pin D4)
- *     10k ohm resistor between power and ground
+ *     4.7k ohm resistor between power and data
  *
  *   OLED:
  *    SDA to GPIO4 (nodeMCU pin D2)
@@ -34,14 +41,17 @@
  *    D2 (nodeMCU GPIO4) to OLED SCL
  *    D4 to ds18b20 data pin
  *    3V3  to power rail on breadboard
- *    GND  to ground rail on breaddboard
+ *    GND  to ground rail on breadboard
+ *
+ *   optional wifi disable jumper:
+ *    D6 (nodeMCU GPIO12) to ground
  *
  * ----------------------------------------------
  *
  * Software used here:
  *     Arduino IDE 1.8.5 on a Macbook Air
  *        at least one driver for the Air to be able to load the nodeMCU
- *        but which one has been list to the sands of time...
+ *        over usb-to-serial port but which one has been list to the sands of time...
  *     Adafruit SSD1306 library version 1.1.2
  *     Adafruit GFX library version 1.2.7
  *
@@ -59,7 +69,8 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#include <NTPClient.h>
+//#include <NTPClient.h>
+
 #include <WiFiUDP.h>
 
 #include <Time.h>          // https://github.com/PaulStoffregen/Time
@@ -80,10 +91,10 @@
  * on internet used LED_BUILTIN to make this work, which worked for me
  * so I used it here too.  Unfortunately I lost the page/author of the
  * actual smarts (sorry) so I can't credit them with the wizardry.
- * 
+ *
  * unfortunately this will blink the onboard bright-blue LED every time
- * the screen refreshes.....
- * 
+ * the screen refreshes, but that does serve as a heartbeat indicator.....
+ *
 */
 
 #define OLED_RESET LED_BUILTIN
@@ -97,9 +108,9 @@ Adafruit_SSD1306 display(OLED_RESET);
 //
 
 #define PROGRAM_NAME "vds-duino-test"    // so I can tell what is loaded on the nodeMCU long after the fact.
-#define PROGRAM_VER  "5"                 // Based on a great idea from reddit user /u/blimpway in /r/esp8266
+#define PROGRAM_VER  "6"                 // Based on a great idea from reddit user /u/blimpway in /r/esp8266
 
-const int DELAY_MS = 15000;               // how often to publish in ms
+const int DELAY_MS = 15000;              // how often to publish in ms
 
 const char* mqttServer = "192.168.1.24"; // FQDN works too, but this saves DNS lookups
 const int mqttPort = 1883;
@@ -108,14 +119,16 @@ const int mqttPort = 1883;
 
 #define ONE_WIRE_BUS 2                   // nodemcu pin D4 the ds18b20 data pin is connect to
 
-#define LED D0                           // status LED we blink - NodeMCU pin GPIO16 (D0).
+#define LED D0                           // status LED we blink - NodeMCU pin GPIO16 (D0)
+
+#define WIFI_ENABLE_PIN D6               // open=enable wifi, ground=disable - NodeMCU pin GPIO12 (D6)
 
 //
 // Stop editing here
 //
 //-----------------------------------------------------
 
-OneWire oneWire(ONE_WIRE_BUS); 
+OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 WiFiClient espClient;
@@ -131,14 +144,17 @@ const char * mqttTopic = together.c_str();
 
 // similarly lets do a unique client id based on chip id
 String clientPrefix         = "esp-";
-String clientTogether             = clientPrefix + chipid;
+String clientTogether       = clientPrefix + chipid;
 const char * mqttClientName = clientTogether.c_str();
+
+// so we can reference this later
+int wifiEnabled = 1;
 
 /*
  * set up the wifi connection
  *
 */
-  
+
 void setup_wifi() {
   delay(1000);
   Serial.print("Connecting to WiFi..");
@@ -169,7 +185,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println("-----------------------");
 }
 
-void setup_mqtt() { 
+void setup_mqtt() {
   client.setServer(mqttServer,mqttPort);                      // for an open broker
   // client.setServer(mqttServer,mqttPort,mqttUser,mqttPass)  // if you password protect your broker
   client.setCallback(callback);
@@ -183,7 +199,7 @@ void setup_mqtt() {
       Serial.println(client.state());
       delay(2000);
     }
-  } 
+  }
 }
 
 /*
@@ -191,8 +207,8 @@ void setup_mqtt() {
  *
 */
 
-float read_ds18b20() {  
- sensors.requestTemperatures(); 
+float read_ds18b20() {
+ sensors.requestTemperatures();
  float temp0 = sensors.getTempFByIndex(0);   // index 0 = first ds18b20 on the data bus
  return temp0;
 }
@@ -218,63 +234,73 @@ void printProgramVersion() {
   display.display();
   delay(5000);
 
-  
 }
 
 /*
  * the overall setup routine
  *
 */
- 
+
 void setup() {
 
   Serial.begin(115200);
   delay(5000);
   printProgramVersion();
-  
+
+  pinMode(WIFI_ENABLE_PIN, INPUT_PULLUP);
+
+  // pin grounded reads 0, pin open reads 1
+  wifiEnabled = digitalRead(WIFI_ENABLE_PIN);
+  Serial.print("wifi enabled: ");
+  Serial.println(wifiEnabled);
+
   pinMode(LED, OUTPUT);    // LED pin as output.
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
   delay(2000);
   display.display();  // show Adafruit splash screen initially
-  
+
   delay(2000);
   display.clearDisplay();
   display.display();
 
-  // setup wifi
-  display.setTextSize(0);
-  display.setTextColor(WHITE);
-  display.setCursor(10,1);
-  display.println("connecting to wifi");
-  display.display();
-  
-  delay(2000);
-  
-  setup_wifi();
-  display.setCursor(10,10);
-  display.println("success!");
-  display.setCursor(10,20);
-  display.println(WiFi.localIP());
-  display.display();
-  
-  delay(2000); 
-  display.clearDisplay();
-  display.display();
+  if (wifiEnabled) {
 
-  // setup mqtt 
-  display.setTextSize(0);
-  display.setTextColor(WHITE);
-  display.setCursor(10,1);
-  display.println("connecting to mqtt");
-  display.display();
-  
-  setup_mqtt();
-  
-  delay(2000);
-  display.setCursor(10,10);
-  display.println("success!");
-  display.display();
+    // setup wifi
+    display.setTextSize(0);
+    display.setTextColor(WHITE);
+    display.setCursor(10,1);
+    display.println("connecting to wifi");
+    display.display();
+
+    delay(2000);
+
+    setup_wifi();
+    display.setCursor(10,10);
+    display.println("success!");
+    display.setCursor(10,20);
+    display.println(WiFi.localIP());
+    display.display();
+
+    delay(2000);
+    display.clearDisplay();
+    display.display();
+
+    // setup mqtt
+    display.setTextSize(0);
+    display.setTextColor(WHITE);
+    display.setCursor(10,1);
+    display.println("connecting to mqtt");
+    display.display();
+
+    setup_mqtt();
+
+    delay(2000);
+    display.setCursor(10,10);
+    display.println("success!");
+    display.display();
+
+  }
 
   delay(2000);
   display.clearDisplay();
@@ -288,8 +314,8 @@ void setup() {
   display.display();
 
   sensors.begin();
-  
-  delay(2000);   
+
+  delay(2000);
   display.clearDisplay();
   display.display();
 
@@ -298,14 +324,14 @@ void setup() {
   display.setCursor(10,1);
   display.println("done setup");
   display.display();
-  
+
   delay(2000);
   display.clearDisplay();
   display.display();
   printProgramVersion();
-  
+
   Serial.println("done setup...");
-  delay(2000); 
+  delay(2000);
   display.clearDisplay();
   display.display();
 
@@ -320,13 +346,13 @@ void loop() {
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
 
-  root["espID"]     = chipid.toInt(); 
+  root["espID"]     = chipid.toInt();
   root["timestamp"] = now();          // this is really 'uptime' since card reset
-  
+
   float degF = read_ds18b20();
   if ((degF > -20) && (degF < 140)) {
     root["degF"] = degF;
-    delay(2000);   
+    delay(2000);
     display.clearDisplay();
     display.setTextSize(0);
     display.setTextColor(WHITE);
@@ -335,12 +361,12 @@ void loop() {
     display.print(degF);
     display.println(" F");
     display.display();
-   
+
   } else {
     Serial.print("error detecting degF: ");
     Serial.println(degF);
-      
-   delay(2000);   
+
+   delay(2000);
    display.clearDisplay();
    display.display();
 
@@ -351,41 +377,44 @@ void loop() {
    display.setCursor(45,20);
    display.println(degF);
    display.display();
-  
+
    delay(5000);
    return;                // hopefully this prevents publishing no value for degF
   }
 
-  char JSONmessageBuffer[200];
-  root.printTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-  
-  // MQTT will drop you occasionally due to session timeout
-  // so be sure to re-establish as needed
-  if (!client.connected()) {
-    Serial.println("mqtt needs to reconnect...");
-    Serial.println(JSONmessageBuffer);   // so we see the payload including timestamp
-    setup_mqtt();
-    return;                     // hopefully we just re-loop() after MQTT comes up
+  if (wifiEnabled) {
+          char JSONmessageBuffer[200];
+          root.printTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+
+          // MQTT will drop you occasionally due to session timeout
+          // so be sure to re-establish as needed
+          if (!client.connected()) {
+            Serial.println("mqtt needs to reconnect...");
+            Serial.println(JSONmessageBuffer);   // so we see the payload including timestamp
+            setup_mqtt();
+            return;               // hopefully we just re-loop() after MQTT comes up
+          }
+
+          // do the actual MQTT publishing
+          //  - if it fails, report to Serial output
+          //  - if the card thinks wifi is disconnected, just restart the card
+          //        (symptoms are system responds to pings, but mqtt never reconnects)
+
+          if (client.publish(mqttTopic, JSONmessageBuffer) == true) {
+                blinkPattern(100,100,1);   // a little positive feedback via onboard LED
+            } else {
+                Serial.println("Error sending mqtt message");
+                Serial.println(JSONmessageBuffer); // so we see the payload including timestamp
+                blinkPattern(1000,1000,2);
+                if (WiFi.status() != WL_CONNECTED) {
+                   Serial.println("restarting - wifi needs to reconnect in loop()...");
+                   ESP.restart();
+                }
+                setup_mqtt();
+                return;
+            }
+
   }
-
-  // do the actual MQTT publishing
-  //  - if it fails, report to Serial output
-  //  - if the card thinks wifi is disconnected, just restart the card
-  //        (symptoms are system responds to pings, but mqtt never reconnects)
-
-  if (client.publish(mqttTopic, JSONmessageBuffer) == true) {
-        blinkPattern(100,100,1);   // a little positive feedback via onboard LED
-    } else {
-        Serial.println("Error sending mqtt message");
-        Serial.println(JSONmessageBuffer); // so we see the payload including timestamp
-        blinkPattern(1000,1000,2);
-        if (WiFi.status() != WL_CONNECTED) {
-           Serial.println("restarting - wifi needs to reconnect in loop()...");
-           ESP.restart();
-        }
-        setup_mqtt();
-        return;
-    }
 
   delay(DELAY_MS);
 
